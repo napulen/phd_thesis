f27ca74567954aef12fe021d0f38b3797dd806e1
Néstor Nápoles López <napulen@gmail.com>
2022-06-14 14:13:07 -0400

    Revert "Updating toc."
    This reverts commit 0ec57734c8cc456441039906a9d19170c4360751.

--- table_of_contents.py
+++ table_of_contents.py
@@ -156,9 +156,9 @@ model_design = """
             19 two-hot encoding
         encoding duration
     the bass and chroma convolutional blocks
-        bass19
-        chromagram19
-        duration14
+        Bass19
+        Chromagram19
+        Duration14
         convolutional layers
             kernel size
             number of filters
